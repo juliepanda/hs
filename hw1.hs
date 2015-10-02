5793af873ba3d6f73842c6c2de0e872272cf05d5
Julie Pan <ycp217@nyu.edu>
2015-10-02 17:06:11 -0400

    solve hanoi

--- hw1.hs
+++ hw1.hs
@@ -1,22 +1,32 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW01 where
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the last digit from a number
 lastDigit       :: Integer -> Integer
 lastDigit x = read xs :: Integer
     where xs = [last (show x)]
 
+-- Drop the last digit from a number
 dropLastDigit   :: Integer -> Integer
 dropLastDigit x = read xs :: Integer
     where xs = init (show x)
 
+-- Exercise 2 -----------------------------------------
 toRevDigits     :: Integer -> [Integer]
-toRevDigits 0 = []
 toRevDigits x
-    | x < 0 = []
+    | x <= 0 = []
     | x > 0 =  x `mod` 10 : toRevDigits (x `div` 10)
 
+-- Exercise 3 -----------------------------------------
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther [] = []
 doubleEveryOther (x:xs:xss) = x : xs*2 : doubleEveryOther xss
 
-
+-- Exercise 4 -----------------------------------------
+--
+-- -- Calculate the sum of all the digits in every Integer.
 sumDigits       :: [Integer] -> Integer
 sumDigits [] = 0
 sumDigits (x:xs)
@@ -30,6 +40,9 @@ addDigs x
     | x >= 10     = x `mod` 10 + addDigs (x `div` 10)
     | otherwise   = x
 
+-- Exercise 5 -----------------------------------------
+--
+-- -- Validate a credit card number using the above functions.
 luhn          :: Integer -> Bool
 luhn 0 = False
 luhn x
@@ -38,3 +51,12 @@ luhn x
     | otherwise     = False
         where credify = (sumDigits (doubleEveryOther (toRevDigits x) ) ) `mod` 10
 
+-- Towers of Hanoi for three pegs
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c
+    | n == 0      = []
+    | n > 1       = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
+    | n == 1      = [(a, b)]
